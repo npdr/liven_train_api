c7ccd65ff11bc81b9df529da7d11de340136ddcc
npdr <p.nieuwenhoff@gmail.com>
2021-08-16 15:24:51 -0300

    reduced 1 more route

--- controllers/address.js
+++ controllers/address.js
@@ -22,37 +22,14 @@ class AddressController {
         }
     }
 
-    async getAddressByFieldInRange(req, res) {
-        try {
-            const address = await Address.query()
-                .select('state', 'city', 'street', 'number', 'value')
-                .where(req.query.field, '>=', req.query.min)
-                .where(req.query.field, '<=', req.query.max);
-
-            return res.status(200).send(address);
-        } catch (err) {
-            res.status(500).send(err);
-        }
-    }
-
     async getAddressByField(req, res) {
         try {
-            const qry = Object.keys(req.query);
-            if (qry.length > 1) {
-                const address = await Address.query()
-                    .select('state', 'city', 'street', 'number', 'value')
-                    .where(req.query.field, '>=', req.query.min)
-                    .where(req.query.field, '<=', req.query.max);
+            const field = Object.keys(req.query)[0];
+            const address = await Address.query()
+                .select('state', 'city', 'street', 'number', 'value')
+                .where(field, '=', req.query[field]);
 
-                return res.status(200).send(address);
-            } else {
-                const field = qry[0];
-                const address = await Address.query()
-                    .select('state', 'city', 'street', 'number', 'value')
-                    .where(field, '=', req.query[field]);
-                return res.status(200).send(address);
-            }
-
+            return res.status(200).send(address);
         } catch (err) {
             res.status(500).send(err);
         }
